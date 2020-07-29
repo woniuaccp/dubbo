6ddf8f08ac4e4106e1183de146742391b1da0c01
wzhui <wangzehuiaccp@163.com>
2020-07-29 20:15:24 +0800

    添加注释BBBB

--- user-web/src/main/java/com/woniu/user/WebApplication.java
+++ user-web/src/main/java/com/woniu/user/WebApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ImportResource;
 /**
  * @author zehui
  * @version 1.0
- * @description TODO
+ * @description TODO 启动类-B
  * @date 2020-07-04 22:40
  */
 @SpringBootApplication
